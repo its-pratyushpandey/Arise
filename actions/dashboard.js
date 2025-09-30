f9ede194c096766779d2ee245ffed0bdac9407af
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-09-30 23:54:26 +0530

    Refactor dashboard.js for AI insights generation

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -1,12 +1,9 @@
 "use server";
-
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
 export const generateAIInsights = async (industry) => {
   const prompt = `
           Analyze the current state of the ${industry} industry and provide insights in ONLY the following JSON format without any additional notes or explanations:
